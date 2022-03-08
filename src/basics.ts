d9b8ff942580a1af62750d8278d987f8059339d7
perfumelim <perfumelim@gmail.com>
2022-03-08 22:24:31 +0900

    410

--- src/basics.ts
+++ src/basics.ts
@@ -47,3 +47,27 @@ function add(a: number, b: number) {
 function print(value: any) {
   console.log(value);
 }
+
+//Generics
+
+// function insertAtBeginning(array: any[], value: any) {
+//   const newArray = [value, ...array];
+//   return newArray;
+// }
+
+//위와 같이 하면 value가 앞에 추가된 새로운 배열을 만들 수 있다. 기존의 배열은 바뀌지 않고.
+// 하지만 위의 타입대로라면 타입스크립트가 타입에러를 잡아주는 효과를 전혀 누릴 수 없다.
+// any[]가 아니라 number[]로 바꾸면 아래처럼 demoArray 같은 건 넣을 수 없게 된다.
+// 그래서 쓰이는게 Generic!
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+//이렇게 하면 value와 같은 타입을 가진 것들의 배열이라는 것을 알려줄 수 있다.
+
+const demoArray = [1, 2, 3];
+
+const updateArray = insertAtBeginning(demoArray, 0); // [0,1,2,3]
+const stringArray = insertAtBeginning(["a", "b", "c"], "d");
