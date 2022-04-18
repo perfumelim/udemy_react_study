cfb2ad4ee1c08c58d91d502b4b8db17868ac09c0
perfumelim <perfumelim@gmail.com>
2022-04-18 23:37:34 +0900

    31~32

--- basic_react/src/components/ExpenseItem.js
+++ basic_react/src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+  return (
+    <>
+      <div>2022/04/18</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </>
+  );
 }
 
 export default ExpenseItem;
