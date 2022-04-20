a7cd9906064d3795677ac52b177fc5429df80215
perfumelim <perfumelim@gmail.com>
2022-04-20 23:56:06 +0900

    32~34

--- basic_react/src/components/ExpenseItem.js
+++ basic_react/src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
 import React from "react";
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
     <>
-      <div>2022/04/18</div>
-      <div>
-        <h2>Car Insurance</h2>
-        <div>$294.67</div>
+      <div className="expense-item">2022/04/18</div>
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </>
   );
