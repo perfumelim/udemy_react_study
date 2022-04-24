8d578a3933c33d02104520eaedb2d31350951ed3
perfumelim <perfumelim@gmail.com>
2022-04-24 23:34:01 +0900

    35~36

--- basic_react/src/components/ExpenseItem.js
+++ basic_react/src/components/ExpenseItem.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
   return (
     <>
-      <div className="expense-item">2022/04/18</div>
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+      <div className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
       </div>
     </>
   );
